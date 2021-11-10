cf95034380e97cb7bdd924a53e9bb1213173ae7f
fmint <fmint@student.21-school.ru>
2021-11-10 15:13:21 +0300

    dddd

--- main.c
+++ main.c
@@ -2,3 +2,12 @@ int main()
 {
 	printf("hello world\n");
 }
+__GCC_ATOMIC_TEST_AND_SET_TRUEVALasd
+__GCC_ATOMIC_TEST_AND_SET_TRUEVALas
+defaultasd
+asmdas
+defaultasd
+staticdas
+defaultasd
+asmdas
+d
